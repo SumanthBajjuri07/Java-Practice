f8c5cab63f88916919317be89a408bea86e15bc5
Sumanth Bajjuri <73301202+SumanthBajjuri07@users.noreply.github.com>
2024-04-22 19:55:05 +0530

    Update Recursion2.java

--- Recursion2.java
+++ Recursion2.java
@@ -1,1 +1,21 @@
+package com.example;
+
+public class User5 {
+	
+	
+	public void printNumbers(int i , int n ){
+        if(i > n ){
+            return ;
+        }
+        printNumbers(i+1, n);
+        System.out.println(i);
+    }
+	
+	
+	public static void main(String[] args) {
+	    User5 m = new User5();
+		m.printNumbers(0, 10);
+		
+	}
+}
 
