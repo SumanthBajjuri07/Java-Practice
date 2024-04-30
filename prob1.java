66f07eeca08e5cc9370adcb1d23511be7923da24
Sumanth Bajjuri <73301202+SumanthBajjuri07@users.noreply.github.com>
2024-04-30 08:42:32 +0530

    Create prob1.java

---
+++ prob1.java
@@ -0,0 +1,42 @@
+// number of even digits numbers problem
+
+
+
+
+
+package com.example;
+
+public class a2 {
+	
+	public static int numberOfDigits(int num) {
+		int count =0;
+		
+		while(num > 0) {
+			count++;
+			num = num / 10;
+		}
+		
+		return count;
+	}
+	
+	
+	public static int evenOrOdd(int count) {
+		if(count % 2 == 0 ) {
+			return 1;
+		}
+		else 
+			return 0;
+	}
+	
+	public static void main(String[] args) {
+		
+	 int num = 1423;
+	 
+	 int count = numberOfDigits(num);
+	 System.out.println(count);
+	 int ans = evenOrOdd(count);
+	 System.out.println(ans);
+	 
+	}
+
+}
